908d307d24076d8356d48bbdaaa7a7f247f688e7
Smsoft007 <muindo25@gmail.com>
2021-08-31 16:19:09 +0900

    title gitup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    asdfasdfasdfasdf
+    수정 title tnwjd
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
